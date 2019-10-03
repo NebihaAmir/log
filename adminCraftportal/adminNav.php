c79be4d7819c67dbb9c9cb2c655877d8ce20e4be
NebihaAmir <craftsoftwarenebiha@gmail.com>
2019-10-03 15:00:09 +0300

    hh

---
+++ adminCraftportal/adminNav.php
@@ -0,0 +1,12 @@
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="userList.php">Admin Employee Mangement Portal</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="adminLogout.php">Logout</a></li>
+      
+    </ul>
+  </div>
+</nav>
